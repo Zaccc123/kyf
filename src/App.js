0e0ad47f0b95de66432e774ae7b0fb6eeccd6746
Zac Kwan <weihao09@gmail.com>
2020-12-14 21:51:05 +0800

    Added AntDesign Chart

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import { Row, Col } from "antd";
 import SideBar from "./containers/SideBar";
-import logo from "./logo.svg";
+import MainChart from "./containers/MainChart";
 import "./App.css";
 
 function App() {
   return (
     <Row>
       <Col span={18} push={6}>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-        </header>
+        <MainChart />
       </Col>
       <Col span={6} pull={18}>
         <SideBar />
